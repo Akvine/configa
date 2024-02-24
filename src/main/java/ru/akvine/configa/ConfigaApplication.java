90439a4f37a0630a71c8d6970d90e3e5093659da
akvine <redgooseman55@gmail.com>
2024-02-24 11:51:45 +0300

    git init

---
+++ src/main/java/ru/akvine/configa/ConfigaApplication.java
@@ -0,0 +1,13 @@
+package ru.akvine.configa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akvine/configa/ConfigaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.configa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
